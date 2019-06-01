52f84a79aa259503da801f45883d89a630cc584b
Andrei Bocan <zmaxor@gmail.com>
2019-06-01 13:20:24 -0700

    Volunteer boilerplate

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CurrentProfileComponent } from '../current-profile/current-profile.component';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
+})
+export class TopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/volunteer-list/volunteer-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { volunteers } from '../volunteers';
+
+@Component({
+  selector: 'app-volunteer-list',
+  templateUrl: './volunteer-list.component.html',
+  styleUrls: ['./volunteer-list.component.css']
+})
+export class VolunteerListComponent {
+  volunteers = volunteers;
+
+  share() {
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
