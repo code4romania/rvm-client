ed37da44ace82c6171b0310fe5d8843fb7a5c6d2
Mercea Emil <mercea.emil@boemeto.com>
2019-07-28 11:47:42 +0300

    Fix env.dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	env: 'DEV',
 	production: false,
-	serverBaseUrl: 'http://192.168.0.110',
-	serverUrl: 'http://192.168.0.110/api'
+	serverBaseUrl: 'https://rvm-api.lxhost.ro',
+	serverUrl: 'https://rvm-api.lxhost.ro/api'
 };
