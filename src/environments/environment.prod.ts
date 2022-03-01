d77e27d159a2860be17560e3586eca235250ff66
Robert Ardeleanu <robert.ardeleanu86@gmail.com>
2022-03-01 22:11:11 +0200

    Revert prod enviroment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	env: 'PROD',
 	production: true,
-	serverBaseUrl: 'https://rvm-api.heroesof.tech',
-	serverUrl: 'https://rvm-api.heroesof.tech/api'
+	serverBaseUrl: 'https://rvm-api.lxhost.ro',
+	serverUrl: 'https://rvm-api.lxhost.ro/api'
 };
