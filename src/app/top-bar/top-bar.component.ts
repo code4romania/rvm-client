89ac391d781bf47f92a2698901ef3cf16790f313
minodora-c <minodora.cristescu@campus.tu-berlin.de>
2022-03-03 11:44:07 +0100

    responsive toolbar
    adjusted the toolbar so that parts of the menu gets hidden when the screen is smaller and there are many toolbar items

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -13,31 +13,87 @@ export class TopBarComponent implements OnInit {
 	 * top bar to be shown over all the other components
 	 */
 	constructor(public authService: AuthenticationService,
-		private router: Router) {}
+		private router: Router) { }
 
-  ngOnInit() { }
+	ngOnInit() { }
 
-  /**
-	 * logout from account
-	 */
+	ngAfterViewInit() {
+		this.onResized();
+	}
+
+	onNavBtn() {
+		/*
+		var hlinks = document.getElementById("ulHiddenLinks");
+		if (hlinks.className.includes("d-none")) {
+			hlinks.classList.remove("d-none")
+		} else {
+			hlinks.classList.add("d-none")
+		}
+		*/
+	}
+
+	onResized() {
+		var btn = document.getElementById("greedy-nav-btn");
+		var vlinks = document.getElementById("ulVisibleLinks");
+		var vlinksChildren = vlinks.getElementsByTagName("li");
+		var hlinks = document.getElementById("ulHiddenLinks");
+		var hlinksChildren = hlinks.getElementsByTagName("li");
+
+		var numVisibleItems = 0;
+		var totalSpace = vlinks.clientWidth;
+		var usedSpace = 0;
+		var remainingSpace = totalSpace - usedSpace - 20;
+
+		// get all links in one array
+		for (var k = 0; k < hlinksChildren.length; k++) {
+			vlinks.append(hlinksChildren[0]);
+		}
+
+		// set visible links
+		for (var i = 0; i < vlinksChildren.length; i++) {
+			if (vlinksChildren[i].clientWidth < remainingSpace) {
+				usedSpace += vlinksChildren[i].clientWidth;
+				remainingSpace = totalSpace - usedSpace;
+				numVisibleItems++;
+			} else {
+				break;
+			}
+		}
+
+		// set hidden links
+		for (var j = vlinksChildren.length - 1; j >= numVisibleItems; j--) {
+			hlinks.prepend(vlinksChildren[j]);
+		}
+
+		// TODO update button
+		var divHiddenLinks = document.getElementById("divHiddenLinks");
+		var height = hlinksChildren.length * 60 + 20 + 'px';
+		divHiddenLinks.style.height = height;
+
+	}
+
+	/**
+	   * logout from account
+	   */
 	logout() {
 		this.authService.logout().subscribe(
-		(didlogout: Boolean) => {
-			if (didlogout) {
-				this.router.navigate(['/login']);
-			}
-		},
-		(error: any) => {
-			console.log('logout error: ', error);
-		});
-  }
+			(didlogout: Boolean) => {
+				if (didlogout) {
+					this.router.navigate(['/login']);
+				}
+			},
+			(error: any) => {
+				console.log('logout error: ', error);
+			});
+	}
 
-  /**
-	 * go to home path on icon click
-	 */
+	/**
+	   * go to home path on icon click
+	   */
 	goToDashboard() {
 		this.router.navigate(['/' + this.authService.homePath()], {
 			replaceUrl: true
 		});
 	}
+
 }
